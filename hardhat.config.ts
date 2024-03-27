24db4730edb6f1e18835be828b1abb314d74c6c1
Christian Palazzo <palazzochristian@yahoo.it>
2024-03-28 00:08:03 +0100

    feat(@script): AG-18 EOA script implementation
    implemented script in order to generate an EOA over different ethereum networks configured

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,11 +6,20 @@ import "solidity-coverage";
 import "hardhat-gas-reporter";
 import "hardhat-deploy";
 
+const GOERLI_URL: string = process.env.GOERLI_URL || "";
+const PRIVATE_KEY: string = process.env.PRIVATE_KEY || "";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   gasReporter: {
     currency: "EUR",
     enabled: process.env.REPORT_GAS ? true : false,
+  },
+  networks: {
+    goerli: {
+      url: GOERLI_URL,
+      accounts: [PRIVATE_KEY],
+    },
   },
 };
 
