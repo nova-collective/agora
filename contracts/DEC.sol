589d41fef264ec536b69ec325bdcce39d9ce4a13
Christian Palazzo <palazzochristian@yahoo.it>
2024-03-29 01:28:24 +0100

    feat(@contracts): AG-19 implemented DEC contract
    implemented DEC contract and related unit tests

--- contracts/DEC.sol
+++ contracts/DEC.sol
@@ -51,26 +51,6 @@ contract DEC {
     }
 
 
-    /// @notice This function is used to decrypt and verify the signature of a DEC
-    function decryptDEC(bytes memory encryptedData, address eoaAddress) public returns (bool) {
-        require(encryptedData.length == 96, "Invalid encrypted data length");
-
-        bytes32 hashedData;
-        bytes memory signature;
-        assembly {
-            hashedData := mload(add(encryptedData, 32))
-            signature := mload(add(encryptedData, 64))
-        }
-
-        address signer = recoverSigner(hashedData, signature);
-        bool success = signer == eoaAddress;
-
-        emit DECDecrypted(eoaAddress, success);
-
-        return success;
-    }
-
-
     /// @notice  This function is used to digitally sign the data
     function signData(bytes32 data) private pure returns (bytes memory) {
         bytes32 hash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", data));
@@ -83,25 +63,5 @@ contract DEC {
     /// @notice this function is used in signData function
     function uintToBytes32(uint256 x) private pure returns (bytes32) {
         return bytes32(x);
-    }
-
-
-    /// @notice This function is used to retrieve the signer of the signed data
-    function recoverSigner(bytes32 dataHash, bytes memory signature) private pure returns (address) {
-        bytes32 r;
-        bytes32 s;
-        uint8 v;
-
-        assembly {
-            r := mload(add(signature, 32))
-            s := mload(add(signature, 64))
-            v := and(mload(add(signature, 65)), 0xff)
-        }
-
-        if (v < 27) {
-            v += 27;
-        }
-
-        return ecrecover(dataHash, v, r, s);
     }
 }
