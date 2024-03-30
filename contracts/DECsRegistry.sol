0dc0d573919cc727c0304dcc2791ffa41a65b238
Christian Palazzo <palazzochristian@yahoo.it>
2024-03-30 15:49:37 +0100

    Ag 20 election smart contract (#9)
    * refactor(@agora): AG-20 switched test network
    
    switched from legacy goerli test network to sepolia
    
    * docs(@docs): AG-20 updated analysis diagrams
    
    updated diagrams in the functional analysis
    
    * feat(@contracts): AG-20 Election smart contract
    
    implemented Election smart contract with unit tests
    
    * feat(@contracts): AG-20 Elections smart contracts
    
    implemeneted different smart contracts for different kind of elections
    
    * docs(@docs): AG-20 addded diagrams files to docs
    
    added diagrams file to the docs folder
    
    * docs(@docs): AG-20 updated class diagrams
    
    updated the smart contracts class diagrams

--- contracts/DECsRegistry.sol
+++ contracts/DECsRegistry.sol
@@ -7,42 +7,45 @@ import "./DEC.sol";
 /// @author Christian Palazzo <palazzochristian@yahoo.it>
 /// @custom:experimental This is an experimental contract.
 contract DECsRegistry is DEC {
-
-    constructor() DEC() {
-        
-    }
+    constructor() DEC() {}
 
     /// @notice this is the list of stamps of elections in which the voter participated
     /// @dev the first address is related to the Voter's EOA, the second array is the Voter's stamps list
-    mapping (address => address[]) electoralStamps;
+    mapping(address => address[]) electoralStamps;
 
     /// @notice this function contains the list of DECs
     /// @dev the address is related to the Voter's EOA
-    mapping (address => bytes) registry;
+    mapping(address => bytes) registry;
 
     event DECRegistered(address indexed voter, bytes dec);
     event DECStamped(address indexed election, address indexed voter);
 
-    
     /// @notice this function is used by the third party authority to register a Voter's DEC in the registry
     /// @dev the DEC contains sensitive data that must be encrypted
     function registerDEC(decData memory dec, address voter) public onlyOwner {
-        require(registry[voter].length == 0, "The Voter's DEC has been already registered");
+        require(
+            registry[voter].length == 0,
+            "The Voter's DEC has been already registered"
+        );
         registry[voter] = encryptDEC(dec);
         emit DECRegistered(voter, registry[voter]);
         return;
     }
 
-
     /// @notice this function returns an encrypted DEC in order to check if a Voter has the voting rights
-    function getDEC(address voter) public view returns(bytes memory) {
-        require(registry[voter].length != 0, "The Voter don't have a registered DEC");
+    function getDEC(address voter) public view returns (bytes memory) {
+        require(
+            registry[voter].length != 0,
+            "The Voter don't have a registered DEC"
+        );
         return registry[voter];
     }
 
-
     /// @notice this function checks in the registry if the Voter already voted in a certail election
-    function hasVoterAlreadyVoted(address voter, address election) public view returns (bool) {
+    function hasVoterAlreadyVoted(
+        address voter,
+        address election
+    ) public view returns (bool) {
         for (uint i = 0; i < electoralStamps[voter].length; i++) {
             if (electoralStamps[voter][i] == election) {
                 return true;
@@ -57,5 +60,5 @@ contract DECsRegistry is DEC {
         electoralStamps[voter].push(election);
         emit DECStamped(election, voter);
         return;
-    } 
+    }
 }
