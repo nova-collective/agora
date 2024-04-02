793c45c9764b01b8760ddcc852fe0c85c1ddc6d9
Christian Palazzo <palazzochristian@yahoo.it>
2024-04-02 23:04:23 +0200

    refactor(@contracts): AG-26 refactored municipality election contract
    refactored municipality election smart contract to make it more gas efficient

--- contracts/MunicipalityElection.sol
+++ contracts/MunicipalityElection.sol
@@ -5,31 +5,31 @@ import "./Election.sol";
 
 /// @title The Municipality Election smart contract
 /// @author Christian Palazzo <palazzochristian@yahoo.it>
+/// @notice This is a kind of Election implementation of the Election mother contract.
 /// @custom:experimental This is an experimental contract.
 contract MunicipalityElection is Election {
     struct Candidate {
         string name;
-        string candidatesFor; // major or councilor
+        string candidatesFor; // majior or councilior
         uint256 points;
     }
 
+    /// @dev the string key of the map correspond tho the party name registered in the parties mapping
     struct Coalition {
         Candidate majorCandidate;
-        string[] parties;
+        mapping(string => bool) parties;
     }
 
-    /// @dev the string is the party name mapped to its list of councilior candidates
-    mapping(string => Candidate[]) public parties;
-    Coalition[] public coalitions;
+    /// @dev the of the mapping is the party name
+    mapping(string => Candidate[]) private parties;
+    Coalition[] private coalitions;
 
-    /// @dev I need this variable to check if the councilior candidate names are unique
+    /// @dev this is a mapping used in private function
     mapping(string => bool) private isCounciliorCandidateInArray;
+    Candidate[] private counciliorCandidatesArray;
 
-    /// @dev I need this variable to assign memory data to storage
-    Candidate[] counciliorCandidatesArray;
-
-    /// @dev it takes track of the parties already in coalition
-    mapping(string => bool) partyExistsInCoalition;
+    /// @dev this is a mapping used in private function
+    mapping(string => bool) private partyExistsInCoalition;
 
     string public municipality;
     string public region;
@@ -52,31 +52,16 @@ contract MunicipalityElection is Election {
         require(
             block.timestamp >= registrationStart &&
                 block.timestamp <= registrationEnd,
-            "This function can be invoked only during the registration period"
+            "This function can only be invoked during the registration period"
         );
         _;
     }
 
-    /**
-     * During the registration period, each party registers to the election, presenting its list of councilor candidates.
-     * The parties registered, can register a coalition of parties, indicating a candidate for the major.
-     * The coalition can also be formed by a single party.
-     * For the definition of the ballot paper they are considered only the coalitions data.
-     */
-
-    /// @notice as first step, we register the parties with their councilor candidates
-    /// @dev only the owner of the contract has write permissions
+    /// @notice as first step, during the registration period the parties register their names and list of councilior candidates
     function registerParty(
         string memory name,
         string[] memory counciliorCandidates
     ) external onlyOwner isRegistrationPeriod {
-        /**
-         * 1. check that the function is invoked in the registration period
-         * 2. check that the party is not already registered;
-         * 3. check that the list of candidates is of 5 elements;
-         * 4. check that the candidates name are unique;
-         * 5. register the party with its candidates in the "parties" variable;
-         */
         require(
             counciliorCandidates.length == 5,
             "The list of councilior candidates must be composed of 5 names"
@@ -90,79 +75,57 @@ contract MunicipalityElection is Election {
             isCounciliorCandidateInArray[counciliorCandidates[i]] = true;
         }
 
-        for (uint i = 0; i < counciliorCandidates.length; i++) {
-            counciliorCandidatesArray[i] = Candidate(
-                counciliorCandidates[i],
-                "councilior",
-                0
-            );
-        }
-
         parties[name] = counciliorCandidatesArray;
     }
 
-    /// @notice the parties already registered can form and register a coalition, indicating the candidate for major
-    /// @dev only the owner of the contract has write permissions
+    /// @notice in this second step, the parties registered compose coalitions and indicate a major candidate name. A coalition is composed by one or more parties.
     function registerCoalition(
-        string memory majorCandidate,
-        string[] memory coalitionParties
-    ) external onlyOwner isRegistrationPeriod {
-        /**
-         * 1. check that the function is invoked in the registration period (consider a modifier); X
-         * 2. check that the list of parties of the coalition are registered in the parties list; x
-         * 3. check that in the list of parties there are no parties already registered in a coalition; x
-         * 4. check that the name of the majorCandidate is unique; x
-         * 5. register the coalition with the name of the majorCandidate;
-         */
-        require(_arePartiesRegistered(parties, coalitionParties), "One or more parties are not registered. Proceed with the registration first");
-        require (!_arePartiesAlreadyInCoalition(coalitions, coalitionParties), "One or more parties are already present in a registered coalition");
-        require(!_isMajorCandidateAlreadyRegistered(majorCandidate, coalitions), "The major candidate is already registered with a coalition");
+    string memory majorCandidate,
+    string[] memory coalitionParties
+) external onlyOwner isRegistrationPeriod {
+    require(_arePartiesRegistered(coalitionParties), "One or more parties are not registered. Proceed with the registration first");
+    require (!_arePartiesAlreadyInCoalition(coalitionParties), "One or more parties are already present in a registered coalition");
+    require(!_isMajorCandidateAlreadyRegistered(majorCandidate), "The major candidate is already registered with a coalition");
 
-        Candidate memory mcandidate = Candidate({
-            name: majorCandidate,
-            candidatesFor: 'major',
-            points: 0
-        }); 
+    Candidate memory mcandidate = Candidate({
+        name: majorCandidate,
+        candidatesFor: 'major',
+        points: 0
+    }); 
 
-        Coalition memory newCoalition = Coalition({
-            majorCandidate: mcandidate,
-            parties: coalitionParties
-        });
-
-        coalitions.push(newCoalition);
+    Coalition storage newCoalition = coalitions[coalitions.length];
+    for (uint i = 0; i < coalitionParties.length; i++) {
+        newCoalition.parties[coalitionParties[i]] = true;
     }
+    newCoalition.majorCandidate = mcandidate;
+}
 
-    function _arePartiesRegistered(mapping(string => Candidate[]) storage registeredParties, string[] memory partiesToCheck) private view returns (bool) {
+    function _arePartiesRegistered(string[] memory partiesToCheck) private view returns (bool) {
         for(uint i = 0; i < partiesToCheck.length; i++) {
-            if (registeredParties[partiesToCheck[i]].length == 0) {
+            if (parties[partiesToCheck[i]].length == 0) {
                 return false;
             }
         }
         return true;
     }
     
-    // Funzione per controllare se le parti sono già in una coalizione
-    function _arePartiesAlreadyInCoalition(Coalition[] memory _coalitions, string[] memory partiesToCheck) private returns (bool) {
-        for(uint i = 0; i < _coalitions.length; i++) {
-            for(uint j = 0; j < _coalitions[i].parties.length; j++) {
-                partyExistsInCoalition[_coalitions[i].parties[j]] = true;
-            }
-        }
-        
-        for(uint i = 0; i < partiesToCheck.length; i++) {
-            if (partyExistsInCoalition[partiesToCheck[i]]) {
-                return true;
+    function _arePartiesAlreadyInCoalition(string[] memory partiesToCheck) private view returns (bool) {
+        for(uint i = 0; i < coalitions.length; i++) {
+            for(uint j = 0; j < partiesToCheck.length; j++) {
+                if (coalitions[i].parties[partiesToCheck[j]]) {
+                    return true;
+                }
             }
         }
         return false;
     }
 
-    function _isMajorCandidateAlreadyRegistered(string memory candidateMajor, Coalition[] memory _coalitions) private pure returns (bool) {
-    for(uint i = 0; i < _coalitions.length; i++) {
-        if (keccak256(abi.encodePacked((_coalitions[i].majorCandidate.name))) == keccak256(abi.encodePacked((candidateMajor)))) {
-            return true;
+    function _isMajorCandidateAlreadyRegistered(string memory candidateMajor) private view returns (bool) {
+        for(uint i = 0; i < coalitions.length; i++) {
+            if (keccak256(abi.encodePacked(coalitions[i].majorCandidate.name)) == keccak256(abi.encodePacked(candidateMajor))) {
+                return true;
+            }
         }
+        return false;
     }
-    return false;
-}
 }
