d8924616e9e3135b8c83de9b4a4be3e4908ffb63
Christian Palazzo <palazzochristian@yahoo.it>
2024-04-07 15:08:33 +0200

    feat(@ci): AG-37 defined ci module
    defined a ci module for the workflows api layer

---
+++ ci/functions.js
@@ -0,0 +1,10 @@
+const functions = {
+  functionOne: function (params) {
+    console.log("FunctionOne invoked with parameters:", params);
+  },
+  functionTwo: function (params) {
+    console.log("FunctionTwo invoked with parameters:", params);
+  },
+};
+
+module.exports = functions;
---
+++ ci/index.js
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/**
+ * This script implements logic delegated from the GitHub Actions workflows.
+ * The workflow invokes the API interface using the CLI with the following command:
+ *
+ *  node ci --function functionOne --params '{"param1": "value1", "param2": "value2"}'
+ */
+const functions = require("./functions");
+
+// process command line arguments
+const args = process.argv.slice(2);
+
+// checks the arguments
+if (args.length < 2 || args[0] !== "--function") {
+  console.error(
+    'Correct usage: node ci --function <functionName> --params \'{"param1": "value1", "param2": "value2"}\'',
+  );
+  process.exit(1);
+}
+
+const functionName = args[1];
+const params = JSON.parse(args[3]);
+
+// Execute the function invoked
+try {
+  functions[functionName](params);
+} catch (e) {
+  console.error("Error: the function invoked does not exists");
+}
